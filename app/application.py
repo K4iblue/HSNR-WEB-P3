1deb80efb9fa39f148a9d842041b6e0b32efa35b
Erik Simon <erik.simon.me@gmail.com>
2020-11-25 14:58:09 +0100

    added controller methods

--- app/application.py
+++ app/application.py
@@ -6,4 +6,37 @@ class Application():
     def default(self):
         raise cherrypy.HTTPError(404)
     default.expose = True
+
+    @cherrypy.expose
+    def index(self):
+        return
+
+    @cherrypy.expose
+    def editEmployees(self):
+        return
+
+    @cherrypy.expose
+    def editTrainings(self):
+        return
+
+    @cherrypy.expose
+    def participationEmployees(self):
+        return
+
+    @cherrypy.expose
+    def participationTrainings(self):
+        return
+
+    @cherrypy.expose
+    def reportEmployees(self):
+        return
+
+    @cherrypy.expose
+    def reportTrainings(self):
+        return
+
+    @cherrypy.expose
+    def reportCertificates(self):
+        return
+
 # EOF
