015dfc4f9231a36c4929411418131848797cd428
Erik Simon <erik.simon.me@gmail.com>
2020-12-09 15:48:14 +0100

    employee_api

--- app/employee_api.py
+++ app/employee_api.py
@@ -2,6 +2,8 @@
 import cherrypy
 from .db.database import Database
 from .model.employee import Employee
+from .model.employee import EmployeeOwnsCertificate
+from .model.employee import EmployeeOwnsQualification
 
 class EmployeeApi:
     def __init__(self, employees: Database, owned_certificates: Database, owned_qualifications: Database):
@@ -36,6 +38,24 @@ class EmployeeApi:
         employee.degree = input_json['degree']
         employee.occupation = input_json['occupation']
         self.employees.insert(employee)
+
+    @cherrypy.expose
+    @cherrypy.tools.json_in()
+    def add_certificate(self):
+        input_json = cherrypy.request.json
+        owned_certificate = EmployeeOwnsCertificate()
+        owned_certificate.employee_id = input_json['employee_id']
+        owned_certificate.certificate_id = input_json['certificate_id']
+        self.owned_certificates.insert(owned_certificate)
+
+    @cherrypy.expose
+    @cherrypy.tools.json_in()
+    def add_qualification(self):
+        input_json = cherrypy.request.json
+        owned_qualification = EmployeeOwnsQualification()
+        owned_qualification.employee_id = input_json['employee_id']
+        owned_qualification.qualification_id = input_json['qualification_id']
+        self.owned_qualifications.insert(owned_qualification)
 
     @cherrypy.expose
     def delete(self, index):
