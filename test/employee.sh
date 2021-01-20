b7d6ed76dea25dc2de093da72450dab3e38046c3
Erik Simon <erik.simon.me@gmail.com>
2021-01-20 14:31:43 +0100

    certificates tests

--- test/employee.sh
+++ test/employee.sh
@@ -6,4 +6,4 @@ curl --fail 127.0.0.1:8080/api/employee/insert \
     -D - \
     -H 'Content-Type: application/json' \
     --data-binary '{"name":"Test","surname":"Test","degree":"Test","occupation":"Test"}'
-curl --fail 127.0.0.1:8080/api/employee/delete
+curl --fail 127.0.0.1:8080/api/employee/delete/1
