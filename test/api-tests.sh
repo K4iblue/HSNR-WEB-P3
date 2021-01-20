9129046853e29f7944721fd7b2a7f2eeb91e597d
MasterGordon <gologo.x@gmail.com>
2021-01-20 14:34:09 +0100

    added tests

--- test/api-tests.sh
+++ test/api-tests.sh
@@ -1,4 +1,7 @@
 set -euo pipefail
 set -v
 
+source ./test/api.sh
 source ./test/training.sh
+source ./test/employee.sh
+source ./test/certificate.sh
--- test/certificate.sh
+++ test/certificate.sh
@@ -5,5 +5,5 @@ curl --fail 127.0.0.1:8080/api/certificate/update \
 curl --fail 127.0.0.1:8080/api/certificate/insert \
     -D - \
     -H 'Content-Type: application/json' \
-    --data-binary '{"title":"Test","desc":"Test","qualifies":"Test",}'
+    --data-binary '{"title":"Test","desc":"Test","qualifies":"Test"}'
 curl --fail 127.0.0.1:8080/api/certificate/delete/1
